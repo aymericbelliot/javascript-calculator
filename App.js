891a90e4d25c20eead169b57c52a29cbf46615c6
aymericbelliot <aymericbelliot@MacBook-de-Aymeric.local>
2019-12-01 00:53:54 -0400

    Nearly finished

--- App.js
+++ App.js
@@ -21,20 +21,41 @@ class App extends React.Component {
         { id: 'divide', value: '/' },
         { id: 'equals', value: '=' }
       ],
-      result: "0",
-      operator: ""
+      output: "0",
+      input: "",
+      hasResult: false
     }
   }
 
+  parseExpr = (expr) => {
+    let result = expr.split('x')
+      .map(expr1 => expr1.split('/')
+        .map(expr2 => expr2.split('-')
+          .map(expr3 => expr3.split('+')
+            .reduce((a, b) => (Number(a) + Number(b)))
+          )
+          .reduce((a, b) => (Number(a) - Number(b)))
+        )
+        .reduce((a, b) => (Number(a) / Number(b)))
+      )
+      .reduce((a, b) => (Number(a) * Number(b)));
+    return result;
+  }
+
   clickPad = (id) => {
-    let previousResult = this.state.result;
-    let newResult = this.state.result;
+    let previousOutput = this.state.output;
+    let newOutput = this.state.output;
+    let previousInput = this.state.input;
+    let newInput = this.state.input;
+    let hasResult = this.state.hasResult;
     let currentValue = this.state.pad.filter(element => element.id == id)[0].value;
 
     switch (id) {
       case "clear":
-        newResult = "0";
+        newOutput = "0";
+        newInput = "";
         break;
+
       case "zero":
       case "one":
       case "two":
@@ -45,28 +66,67 @@ class App extends React.Component {
       case "seven":
       case "height":
       case "nine":
-        (previousResult != "0") ?
-          newResult = previousResult.toString().concat(currentValue)
-          : newResult = previousResult.toString().concat(currentValue).slice(1);
+        if (hasResult) {
+          newOutput = currentValue; // reset calculator
+          newInput = "";
+        } else {
+          newOutput = previousOutput.concat(currentValue); // enter numbers
+        }
+        hasResult = false;
         break;
+
       case "decimal":
-        (!RegExp(/\./).test(previousResult)) ?
-          newResult = previousResult.toString().concat(currentValue)
-          : newResult = previousResult;
+        if (hasResult) {
+          newOutput = "0".concat(currentValue); // reset calculator
+          newInput = "";
+        } else if (!RegExp(/\./).test(previousOutput)) { // check if there is already a dot
+          newOutput = previousOutput.concat(currentValue)
+        } else {
+          newOutput = previousOutput;
+        }
+        hasResult = false;
         break;
+
       case "add":
+      case "substract":
+      case "multiply":
+      case "divide":
+        if (hasResult) {
+          console.log("a travailler");
+        }
+        else if (previousOutput == "-") { // we already type some symbol
+          if (currentValue == "-") { // don't make - -
+            previousInput.charAt(previousInput.length - 1) == "-" ?
+              newOutput = previousOutput
+              : newOutput = "-";
+          } else { // replace previous symbol
+            newInput = previousInput.slice(0, previousInput.length - 1).concat(currentValue);
+          }
+        } else { // we entered a new number
+          newOutput = currentValue;
+          newInput = previousInput.concat(previousOutput);
+        }
+        hasResult = false;
+        break;
 
-
-
+      case "equals":
+        newOutput = this.parseExpr(previousInput.concat(previousOutput));
+        newInput = previousInput.concat(previousOutput).concat(currentValue).concat(newOutput);
+        hasResult = true;
+        break;
     }
 
-    this.setState({ result: newResult })
+    this.setState({
+      input: newInput,
+      output: newOutput,
+      hasResult: hasResult
+    });
   }
 
   render() {
     return (
       <div className="App container-fluid">
-        <Calculator pad={this.state.pad} clickpad={this.clickPad.bind(this)} result={this.state.result} />
+        <Calculator pad={this.state.pad} clickpad={this.clickPad.bind(this)} output={this.state.output} input={this.state.input} />
       </div>
     );
   }
@@ -83,7 +143,8 @@ class Calculator extends React.Component {
 
     return (
       <div className="card d-flex flex-column p-3 shadow">
-        <input id="display" type="text" readOnly className="form-control-plaintext bg-secondary text-white text-right p-1" value={this.props.result}></input>
+        <input type="text" readOnly className="form-control-plaintext bg-secondary text-white text-right p-1" value={this.props.input}></input>
+        <input id="display" type="text" readOnly className="form-control-plaintext bg-secondary text-white text-right p-1" value={this.props.output}></input>
         <div className="calcPad">
           {buildPad}
         </div>
